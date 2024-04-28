b70bb74653c6fc6d65b9dd85ad24979060f3d016
JATIN VERMA <jv7889230967@gmail.com>
2024-04-28 23:20:29 +0530

    page-1-issue

--- app/page.js
+++ app/page.js
@@ -17,37 +17,37 @@ export default function Home() {
   }, [router]);
   
   return (
-    <div className="flex overflow-x-hidden h-screen w-screen bg-orange-100">
+    <div className="absolute h-screen w-screen overflow-hidden bg-orange-100">
       <h1 className="flex fixed gap-4 inset-y-12 inset-x-24 text-black text-5xl font-bold"><FaReact /> Astrix.</h1>
 
-      <div className="flex flex-col h-auto w-auto overflow-x-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        
-        <section className="flex flex-row h-20 w-auto gap-3 items-center m-2">  {/*multiple section for displaying text*/}
+      <div className="absolute flex flex-col h-[32rem] w-[73rem] md:overflow-x-visible gap-3 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 border-solid border-black">
+          {/*multiple section for displaying text*/}
+        <section className="flex flex-row h-20 w-[70rem] gap-3 items-center m-2 border-2 border-solid border-black">
           <p className="text-6xl font-bold not-italic">WE</p>
           <div className="w-64 h-16 galaxy-image transition-transform"></div>
           <p className="text-6xl font-bold not-italic">ORGANIZE THE</p>
         </section>
 
-        <section className="flex h-20 w-auto gap-3 items-center m-2">
+        <section className="flex h-20 w-[70rem] gap-3 items-center m-2 border-2 border-solid border-black">
           <p className="text-6xl font-bold not-italic">CONNECTION</p>
           <div className="h-16 w-52 garden-image"></div>
         </section>
 
-        <section className="flex h-20 w-auto gap-3 items-center m-2">
+        <section className="flex h-20 w-[70rem] gap-3 items-center m-2 border-2 border-solid border-black">
           <div className="h-16 w-52 party-image"></div>
           <p className="text-6xl font-bold not-italic">BETWEEN</p>
           <div className="h-16 w-52 singing-image"></div>
           <p className="text-6xl font-bold not-italic">MUSIC</p>
         </section>
 
-        <section className="flex h-20 w-auto gap-3 items-center m-2">
+        <section className="flex h-20 w-[70rem] gap-3 items-center m-2 border-2 border-solid border-black">
           <p className="text-6xl font-bold not-italic">ARTIST</p>
           <div className="h-16 w-52 concert-image"></div>
           <p className="text-6xl font-bold not-italic">CULTURE</p>
           <div className="h-16 w-52 crowd-image"></div>
         </section>
 
-        <section className="flex h-20 w-100% gap-3 items-center m-2">
+        <section className="flex h-20 w-[70rem] gap-3 items-center m-2 border-2 border-solid border-black">
           <div className="h-16 w-96 flowers-image"></div>
           <p className="text-6xl font-bold not-italic">ART</p>
           <div className="h-16 w-52 flowers2-image"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,31 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      keyframes: {
-        slide: {
-          "0%,100%": { transform: "w-80" },
-          "50%": { transform: "w-64" },
-        },
-        slideinRight: {
-          "0%": { transform: "translateX(100%)" },
-          "100%": { transform: "translateX(0px)" },
-        },
-        slideDown: {
-          "0%": { transform: "translateY(-100%)" },
-          "100%": { transform: "translateY(0)" },
-        },
-        slideUp: {
-          "0%": { transform: "translateY(100%)" },
-          "100%": { transform: "translateY(0)" },
-        },
-      }, 
-      animation: {
-        slidein: "slide 0.2s ease-out",
-        slideOut: "slide 0.2s ease-in reverse",
-        slideinRight: "slideinRight 1.5s ease-in-out infinite",
-        slideDown: "slideDown 0.2s ease-out",
-        slideUp: "slideUp 0.2s ease-out",
-      },
+      width: {
+        '128': '80rem',
+      }
     },
   },
   plugins: [
