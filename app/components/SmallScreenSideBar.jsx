8a79c84f0ab507b103aef03b50f975cecf2272f8
JATIN VERMA <jv7889230967@gmail.com>
2024-04-29 00:40:24 +0530

    qr-img-fix

--- app/components/SmallScreenSideBar.jsx
+++ app/components/SmallScreenSideBar.jsx
@@ -33,7 +33,7 @@ export default function SmallScreenSideBar({activetab}) {
         <br/>
         {activetab==='events'?<EventsScroll/>:<ScrollItems/>}
   <div className={`flex flex-row h-40 w-96 ${activetab === 'events' ? 'justify-between' : 'justify-end'} items-end mt-12`}>
-  {activetab === 'events' && <Image src={'/assets/qr-Code.webp'} alt='card' height={100} width={100}/>}
+  {activetab === 'events' && <Image src={'/assets/qr-image.jpg'} alt='qr-image' height={100} width={100}/>}
   <div className='flex items-center justify-center h-14 w-40 mt-14 bg-yellow-300 rounded-full text-xl font-bold'>
     Join WaitList
   </div>
